f5ed38f8d11f9e1b72db47fa1788ecb118e05b82
Reynaldi Ananda Pane <reynaldipane@gmail.com>
2018-04-20 17:57:55 +0700

    Merge Reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { treki } from './treki/treki.reducer';
-
+import userReducer from './user/user.reducer';
 export default store = createStore(
   combineReducers({
     treki,
+    userReducer
   }),
   applyMiddleware(thunk)
 );
